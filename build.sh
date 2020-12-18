8c7cd1e6466acdf966b1b2276fc582b0bb5632b4
miguemesen <miguemesen12@gmail.com>
2020-12-18 16:27:08 -0600

    Bug fix

---
+++ build.sh
@@ -0,0 +1,7 @@
+echo "1/2 Starting the build of the react application ..."
+npm run build
+
+echo "2/2 Packing the application into a docker image ..."
+docker build . -t star-wars-server
+
+echo "2/2 Steps completed. Bye"
