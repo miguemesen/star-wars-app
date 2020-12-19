187b267e27559f08852890c6b30406865ebd777d
miguemesen <miguemesen12@gmail.com>
2020-12-19 09:49:18 -0600

    Added install dependencies command to build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,11 @@
-echo "1/2 Starting the build of the react application ..."
+
+echo "1/3 Getting dependencies ..."
+sudo npm install
+
+echo "2/3 Starting the build of the react application ..."
 sudo npm run build
 
-echo "2/2 Packing the application into a docker image ..."
+echo "2/3 Packing the application into a docker image ..."
 docker build . -t star-wars-server
 
-echo "2/2 Steps completed. Bye"
+echo "3/3 Steps completed. Bye"
