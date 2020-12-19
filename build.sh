6c394547e09d4104ad7eb742dc44e3a33d39c993
miguemesen <miguemesen12@gmail.com>
2020-12-19 10:58:51 -0600

    Push to docker hub if successful

--- build.sh
+++ build.sh
@@ -1,11 +1,15 @@
 
-echo "1/3 Getting dependencies ..."
+echo "1/4 Getting dependencies ..."
 sudo npm install
 
-echo "2/3 Starting the build of the react application ..."
+echo "2/4 Starting the build of the react application ..."
 sudo npm run build
 
-echo "2/3 Packing the application into a docker image ..."
-docker build . -t star-wars-server
+echo "2/4 Packing the application into a docker image ..."
+docker build . -t miguemesen/star-wars-server:latest
 
-echo "3/3 Steps completed. Bye"
+echo "3/4 Packing the application into a docker image ..."
+docker login -u miguemesen -p $DOCKER_HUB_PASSWORD
+docker push star-wars-server:latest
+
+echo "4/4 Steps completed. Bye"
